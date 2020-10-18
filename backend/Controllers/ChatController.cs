d6b0b9ad8ee890f73467071bd4d3bf5fc5a4819c
Dmitry <64337953+d1van4ik@users.noreply.github.com>
2020-10-18 10:10:59 +0300

    Доделаны все действия + Swagger

--- backend/Controllers/ChatController.cs
+++ backend/Controllers/ChatController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
 using BookmakerHouse.Data;
@@ -24,13 +26,33 @@ namespace backend.Controllers
             _logger = logger;
         }
 
+        
+        /// <summary>
+        /// Создать чат
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса:
+        ///
+        ///     POST /create
+        ///     {
+        ///          "name": "string",
+        ///          "lat": 0,
+        ///          "long": 0,
+        ///          "adress": "string"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>Созданная беседа</returns>
+        /// <response code="201">Все ок, беседа создана</response>
+        /// <response code="400">Ошибка парса ответа от апи вк</response>
+        /// <response code="406">Ошибка выполнения запроса на апи вк</response> 
         [HttpPost]
         [Route("[controller]/create")]
         public async Task<IActionResult> Create(Chat chat)
         {
             var @params = new Dictionary<string, string>
             {
-                {"user_ids", chat.CreatorId.ToString()},
+                {"user_ids", string.Empty},
                 {"title", chat.Name}
             };
             var result = await _api.CallAsync("messages.createChat", @params);
@@ -49,6 +71,27 @@ namespace backend.Controllers
             return StatusCode(201, chat);
         }
 
+        /// <summary>
+        /// Получить инвайт-ссылку на беседу
+        /// </summary>
+        /// <param name="id">Id беседы</param>
+        /// <param name="reset">Пересоздавать ли ссылку</param>
+        /// <remarks>
+        /// Пример запроса:
+        ///
+        ///     POST /create
+        ///     {
+        ///          "name": "string",
+        ///          "lat": 0,
+        ///          "long": 0,
+        ///          "adress": "string"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>Ссылка-приглашение в беседу</returns>
+        /// <response code="200">Все ок</response>
+        /// <response code="404">Ошибка от апи вк</response>
+        /// <response code="406">Ошибка выполнения запроса на апи вк</response> 
         [HttpGet]
         [Route("[controller]/getInviteLink")]
         public async Task<IActionResult> GetInviteLink(int id, bool reset = false)
@@ -63,24 +106,85 @@ namespace backend.Controllers
                 return StatusCode(406);
             if (!result.TryGetValue("response", out var response))
             {
-                var error = result.GetValue("error").ToString();
-                _logger.LogWarning(error);
-                return NotFound(error);
+                var error = result.GetValue("error");
+                _logger.LogError(error.ToString());
+                return NotFound(error["error_msg"].Value<string>());
             }
 
             var link = response.Value<string>("link");
             return StatusCode(200, link);
         }
 
+        /// <summary>
+        /// Поиск по адресу
+        /// </summary>
+        /// <param name="adress">Адрес</param>
+        /// <remarks>
+        /// Пример запроса:
+        ///
+        ///     POST /create
+        ///     {
+        ///          "name": "string",
+        ///          "lat": 0,
+        ///          "long": 0,
+        ///          "adress": "string"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>Список бесед</returns>
+        /// <response code="200">Все ок</response>
+        /// <response code="404">Не найдено ничего</response>
+        /// <response code="406">Ошибка обновления данных о чатах</response> 
         [HttpPost]
         [Route("[controller]/getByAdress")]
         public async Task<IActionResult> GetByAdress(string adress)
         {
             var result = await _dbContext.Chats
-                .SingleOrDefaultAsync(a => a.Adress == adress);
+                .Where(a => a.Adress == adress)
+                .ToArrayAsync();
             if (result == null)
                 return NotFound();
+            if(!await UpdateChats(result))
+                return StatusCode(406);
             return Ok(result);
+        }
+
+        //LOAD CHATS ONLY EXISTED IN BASE
+        [NonAction]
+        public async Task<bool> UpdateChats(Chat[] chats)
+        {
+            if(chats==null || !chats.Any())
+                return true;
+            //
+            var @params = new Dictionary<string, string>
+            {
+                {"peer_ids", string.Join(",",chats.Select(c => 2000000000 + c.Id))},
+                {"extended", "1"}
+            };
+            var result = await _api.CallAsync("messages.getConversationsById", @params);
+            if (result == null || !result.TryGetValue("response", out var response))
+            {
+                var error = result?.GetValue("error").ToString();
+                _logger.LogWarning(error);
+                return false;
+            }
+
+            try
+            {
+                for (int i = 0; i < chats.Length; i++)
+                {
+                    chats[i].Name = response["items"][i]["chat_settings"]["title"].Value<string>();
+                    chats[i].UserCount = response["items"][i]["chat_settings"]["members_count"].Value<int>();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e,"Ошибка синхронизации данных о чатах");
+                return false;
+            }
+            _dbContext.Chats.UpdateRange(chats);
+            await _dbContext.SaveChangesAsync();
+            return true;
         }
     }
 }

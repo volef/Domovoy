5b8ada1b8172cef496761344ca4e64ce6578821a
Dmitry <64337953+d1van4ik@users.noreply.github.com>
2020-10-18 10:10:08 +0300

    Донастройка

--- backend/Models/Chat.cs
+++ backend/Models/Chat.cs
@@ -1,15 +1,44 @@
-﻿namespace backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
 {
     public class Chat
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-
-        public int CreatorId { get; set; }
         public decimal lat { get; set; }
         public decimal @long { get; set; }
-
+        [Required]
         public string Adress { get; set; }
         public int UserCount { get; set; }
+
+        protected bool Equals(Chat other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Chat) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+        
+        public static bool operator ==(Chat c1,Chat c2)
+        {
+            return c1.Id == c2.Id;
+        }
+        
+        public static bool operator !=(Chat c1,Chat c2)
+        {
+            return c1.Id != c2.Id;
+        }
     }
 }
